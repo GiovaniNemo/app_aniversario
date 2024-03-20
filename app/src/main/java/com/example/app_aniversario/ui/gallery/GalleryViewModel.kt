6eb9062d47b92ae486e163844e8ecbf5563223b9
Dizinho <adrian_ribeirodias@hotmail.com>
2024-03-19 21:14:03 -0300

    v0.0.1

--- app/src/main/java/com/example/app_aniversario/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/app_aniversario/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = ""
+        value = "teste"
     }
     val text: LiveData<String> = _text
 }
